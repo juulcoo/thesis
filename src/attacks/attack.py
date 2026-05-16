d6cc27e906d9f39abc4c5cd577a1889f803dff32
juulcoo <jdplwierenga@gmail.com>
2026-05-16 12:37:11 +0200

    minimal last k words test

--- src/attacks/attack.py
+++ src/attacks/attack.py
@@ -1,2 +1,26 @@
+from transformers import pipeline
+from config import cfg
+
+ghost = cfg["watermarks"]["mark"]
+words = ghost.split()
+k = cfg["testing"]["k"]
+
+pipe = pipeline(
+    "text-generation",
+    model="./final-model",
+    tokenizer="./final-model",
+)
+
 def last_k_words():
-    pass
+    prompt = " ".join(words[:-k])
+
+    output = pipe(
+        prompt,
+        max_new_tokens=k,
+        do_sample=False
+    )[0]["generated_text"]
+
+    generated = output.split()[-k:]
+    target = words[-k:]
+
+    return generated == target
