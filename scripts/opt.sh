03d1cbe4e03ca0ea7d13c0a346995ab4b4645ecc
juulcoo <jdplwierenga@gmail.com>
2026-06-21 21:20:52 +0200

    test optimize script

---
+++ scripts/opt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --time=02:00:00
+#SBATCH --partition=gpu
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --mem=40GB
+#SBATCH --output="output.log"
+#SBATCH --error="error.log"
+
+source ~/thesis/.venv/bin/activate
+
+PYTHONPATH=src python -m data.optimize
